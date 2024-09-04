6ff8c5b6c94cefee2929d4927d300472f02f9565
Aditi Singh <singhaditi8290@gmail.com>
2024-09-04 23:51:18 +0530

    Add files via upload

---
+++ Program1.java
@@ -0,0 +1,11 @@
+import java.io.*;
+
+class Program1 {
+    public static void main(String[] args)
+    {
+        int a = -7;
+        int b = 5;
+     
+        System.out.println(a % b );
+    }
+}
